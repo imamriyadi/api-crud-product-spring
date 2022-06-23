d104935b460ba37378b93321927e1e887c789fed
Imam Riyadi <imamriyadi@jsm.co.id>
2022-06-23 23:33:26 +0700

    update endpoint delete

--- src/main/kotlin/com/imamriyadi/apicrudproductspring/product/controller/ProductController.kt
+++ src/main/kotlin/com/imamriyadi/apicrudproductspring/product/controller/ProductController.kt
@@ -4,6 +4,7 @@ import com.imamriyadi.apicrudproductspring.BaseResponse
 import com.imamriyadi.apicrudproductspring.product.entity.Product
 import com.imamriyadi.apicrudproductspring.product.service.ProductService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -60,6 +61,17 @@ class ProductController {
             status = true,
             message = "Update Product Success",
             data = productService.updateProduct(ids,product)
+        )
+    }
+
+    @DeleteMapping("/{ids}")
+    fun deleteProduct(
+        @PathVariable(name="ids") ids: String
+    ):BaseResponse<List<Product>>{
+        return BaseResponse(
+            status = true,
+            message = "Delete Success",
+            data = productService.deleteProduct(ids)
         )
     }
 }
